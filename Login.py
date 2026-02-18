77c4fc5b99137ea53420179ba9304219c55cc287
Roopan <sankarroopan@gmail.com>
2026-02-18 23:54:23 +0530

    Update Data driven Testing Testcases

--- Login.py
+++ Login.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+from turtledemo.clock import current_day
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,8 +15,6 @@ username = row['Username']
 password = row['Password']
 print(username)
 print(password)
-
-
     #     if line_count == 0:
     #         print(f'Column names are :{", ".join(row)}')
     #         line_count += 1
@@ -30,7 +30,16 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get(url)
 # locate the elements
+driver.implicitly_wait(60)
 driver.find_element(By.XPATH,"//input[@placeholder='Username']").send_keys(username)
-print(username)
+driver.implicitly_wait(60)
 driver.find_element(By.XPATH,"//input[@placeholder='Password']").send_keys(password)
-print(password)
+driver.implicitly_wait(60)
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
+with open('./Files/csv_example.csv', mode='w') as f:
+    csv_writer = csv.writer(f, delimiter=',')
+    # writing the header
+    # csv_writer.writerow(['Date', 'Time of Test', 'Name of Tester','Test Result'])
+    # writing multiple rows
+    row = {datetime.date:row['Date'],datetime.time():row['Time of Test'],'Tester1':row['Name of Tester'],'Pass':row['Test Result']}
+    csv_writer.writerow(row)
